331b3eaef446f2f896ec2dd631081f8f7a7e1fb8
Lauri Mukkala <lauri.mukkala@qalmari.fi>
2023-09-18 12:07:40 +0300

    Add splash to the sabbat list

--- lib/pages/sabbat_card.dart
+++ lib/pages/sabbat_card.dart
@@ -13,32 +13,42 @@ class SabbatCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Container(
-      padding: const EdgeInsets.all(8),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage('assets/images/${sabbat.name}.jpg'),
-            fit: BoxFit.cover),
+      child: Stack(
+        children: [
+          Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/${sabbat.name}.jpg'),
+                    fit: BoxFit.cover),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.7),
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      sabbat.name,
+                      style: const TextStyle(fontSize: 48),
+                    ),
+                    Text(
+                      '${sabbat.date.day}.${sabbat.date.month}.${sabbat.date.year}',
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+              )),
+          Positioned.fill(
+              child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    splashColor: Colors.red,
+                    onTap: () => {},
+                  )))
+        ],
       ),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.7),
-          borderRadius: const BorderRadius.all(Radius.circular(8)),
-        ),
-        child: Column(
-          children: <Widget>[
-            Text(
-              sabbat.name,
-              style: const TextStyle(fontSize: 48),
-            ),
-            Text(
-              '${sabbat.date.day}.${sabbat.date.month}.${sabbat.date.year}',
-              style: const TextStyle(fontSize: 24),
-            ),
-          ],
-        ),
-      ),
-    ));
+    );
   }
 }
