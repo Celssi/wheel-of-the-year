0574fac260b143995517b1e3485a522a8cb2d17f
Lauri Mukkala <lauri.mukkala@qalmari.fi>
2023-09-26 15:00:10 +0300

    Refactoring

--- lib/pages/home/moon_single_content.dart
+++ lib/pages/home/moon_single_content.dart
@@ -21,8 +21,7 @@ class MoonSingleContent extends StatelessWidget {
       unsortedDates[getLunarPhaseLabel(phaseName)] = nextDate;
     }
 
-    List<MapEntry<String, DateTime>> sortedList =
-        unsortedDates.entries.toList();
+    List<MapEntry<String, DateTime>> sortedList = unsortedDates.entries.toList();
     sortedList.sort((a, b) => a.value.compareTo(b.value));
     Map<String, DateTime> sortedDates = Map.fromEntries(sortedList);
 
